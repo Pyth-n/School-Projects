94a63887823d5551896173873b75f037448b99af
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-05-13 23:25:31 -0700

    More files we'll need

---
+++ week05/week05/set.h
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Header:
+ *    set.h
+ * Summary:
+ *    This will contain the code for our week05 set assignment
+ * Author
+ *    Jeffry Simpson, David Perez
+ ************************************************************************/
+
+#ifndef set_h
+#define set_h
+
+
+#endif /* set_h */
