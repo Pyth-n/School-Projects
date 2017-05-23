40f88496eaf48e5de70df72682333442dc6460bc
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-05-23 07:16:20 -0700

    Updated makefile and headers

---
+++ week06/week06/node.h
@@ -0,0 +1,14 @@
+/***********************************************************************
+ * Header:
+ *    set.h
+ * Summary:
+ *    This will contain the code for our week05 set assignment
+ * Author
+ *    Jeffry Simpson, David Perez
+ ************************************************************************/
+
+#ifndef node_h
+#define node_h
+
+
+#endif /* node_h */
--- week06/week06/sortInsertion.h
+++ week06/week06/sortInsertion.h
@@ -5,7 +5,7 @@
  *    This will contain just the prototype for insertionSortTest(). You may
  *    want to put other class definitions here as well.
  * Author
- *    <your names here>
+ *    Jeffry Simpson, David Perez
  ************************************************************************/
 
 #ifndef INSERTION_SORT_H
