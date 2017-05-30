c4a9cbc278bdd2d39d4aa62f15525ee21bff0872
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-05-29 22:06:10 -0700

    Initial week07 file setup

---
+++ week07/week07/list.h
@@ -0,0 +1,22 @@
+/***********************************************************************
+ * Header:
+ *    set.h
+ * Summary:
+ *    This will contain the code for our week07 List assignment
+ * Author
+ *    Jeffry Simpson, David Perez
+ ************************************************************************/
+
+#ifndef list_h
+#define list_h
+#include <iostream>
+using namespace std;
+
+#include <cassert>
+
+
+
+
+
+
+#endif /* list_h */
