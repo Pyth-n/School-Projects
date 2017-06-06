22ccb92217c111a5612d4bc98d364786d0f1cbc2
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-06-05 22:12:43 -0700

    trying to get it to work

---
+++ week08/week08/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  week08
+//
+//  Created by Jeffry Simpson on 6/5/17.
+//  Copyright © 2017 Jeffry Simpson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
