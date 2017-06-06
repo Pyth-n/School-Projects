49af3b270bb91faf5976fb8d89a8e8fe587782af
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-06-05 22:21:58 -0700

    Commented out test 2,3,4 for now.

--- week08/week08/week08.cpp
+++ week08/week08/week08.cpp
@@ -35,9 +35,9 @@ void testMerge();
 // I suggest first commenting out all of these tests, then try to use only
 // TEST1.  Then, when TEST1 works, try TEST2 and so on.
 #define TEST1   // for testSimple()
-#define TEST2   // for testAdd()
-#define TEST3   // for testDisplay()
-#define TEST4   // for testMerge()
+//#define TEST2   // for testAdd()
+//#define TEST3   // for testDisplay()
+//#define TEST4   // for testMerge()
 
 /**********************************************************************
  * MAIN
