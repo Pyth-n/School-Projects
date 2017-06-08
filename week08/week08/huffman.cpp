fbd893622342dcfde4815724ec236eb49afce723
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-06-07 23:30:25 -0700

    added queue and file input for huffman codes

--- week08/week08/huffman.cpp
+++ week08/week08/huffman.cpp
@@ -11,18 +11,28 @@
 
 #include "huffman.h"       // for HUFFMAN() prototype
 
+
 /*******************************************
  * HUFFMAN
  * Driver program to exercise the huffman generation code
  *******************************************/
 void huffman(string fileName)
 {
-   
+   Queue<HuffHolder> huffmanQue;
+   HuffHolder item;
+   bool flag = true;
    
    
    //Create a huffman class using the pair.h
    
    //read the file into a data structure with the huffman class
+   flag = readFromFile(huffmanQue, fileName);
+   if(!flag)
+   {
+      cout << "Error reading from file\n";
+      return;
+   }
+   
    
    //parse the structure, and create a tree with smallest two items
    
@@ -31,4 +41,55 @@ void huffman(string fileName)
    //display the path info
    
    return;
+}
+
+
+/************************************************
+ * readfromFile
+ * Get input from text file
+ ***********************************************/
+bool readFromFile(Queue<HuffHolder> & huffmanQue, const string fileName)
+{
+   char * nextCard = nullptr;       //NextCard input
+   string stringInput;              //String from file
+   double valueInput;               //double from file
+   HuffHolder curItem;
+   
+   ifstream fin(fileName);          //Open file
+   if (fin.fail())
+   {
+      cout << "Unable to open file " << fileName << endl;
+      return false;
+   }
+   
+   while(!fin.eof())                //While not at End of File
+   {
+      fin >> stringInput;
+      fin >> valueInput;
+      
+      if (fin.eof())
+      {
+         return true;
+      }
+      else if (fin.fail())
+      {
+         cout << "Error reading from file\n";
+         return false;
+      }
+      else
+      {
+         
+         char *cstr = new char[stringInput.length() + 1];      //Create Char variable
+         strcpy(cstr, stringInput.c_str());                    //String copy
+         curItem.name = cstr;
+         //curItem.value = stod(valueInput);
+         curItem.value = valueInput;
+         huffmanQue.push(curItem);                                    //Insert string
+         delete [] cstr;                                       //Delete Char variable
+      }
+      
+   }
+   
+   
+   return true;
 }
--- week08/week08/huffman.h
+++ week08/week08/huffman.h
@@ -14,9 +14,22 @@
 #include <iostream>    // for CIN and COUT
 #include <string>      // for STRING
 #include <cassert>     // for ASSERT
+#include <fstream>
+#include <string.h>
+#include "queue.h"
 
 using namespace std;
 
+struct HuffHolder
+{
+   string name = "", huffcode = "";
+   double value = 0.0;
+};
+
+BinaryNode <Pair<T1, T2>> hTree;
+
 void huffman(string fileName);
+bool readFromFile(Queue<HuffHolder> & huffmanQue, const string fileName);
+
 
 #endif // HUFFMAN_h
