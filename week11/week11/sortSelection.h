b73f178574544b9cd9d4d19ec02c25ab9b1fa682
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-06-26 17:58:35 -0700

    Initial setup for week11

---
+++ week11/week11/sortSelection.h
@@ -0,0 +1,24 @@
+/***********************************************************************
+ * Module:
+ *    Week 11, Sort Select
+ *    Brother Helfrich, CS 235
+ * Authors
+ *    Jeffry Simpson, David Perez
+ * Summary:
+ *    This program will implement the Selection Sort
+ ************************************************************************/
+
+#ifndef SORT_SELECTION_H
+#define SORT_SELECTION_H
+
+/*****************************************************
+ * SORT SELECTION
+ * Perform the selection sort
+ ****************************************************/
+template <class T>
+void sortSelection(T array[], int num)
+{
+}
+
+
+#endif // SORT_SELECTION_H
