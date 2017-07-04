44128a72592cb5aedea8dc63c705c4905c4169c3
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-07-03 20:56:21 -0700

    Initial push of files

---
+++ week12/week12/hash.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Module:
+ *    Week 12, Hash Table
+ *    Brother Helfrich, CS 235
+ * Authors
+ *    Jeffry Simpson, David Perez
+ * Summary:
+ *    This program will implement the Hash Class
+ ************************************************************************/
+
+#ifndef HASH_H
+#define HASH_H
+
+
+
+#endif /* hash_h */
