0cab5745a21a0ff51c220047787443380b89973c
Jeffry Simpson <jeffrysimpson@gmail.com>
2017-07-11 07:26:38 -0700

    Added additional header files from Bro Jones post in discussion boards

---
+++ week13/week13/graph.h
@@ -0,0 +1,27 @@
+/***********************************************************************
+ * Component:
+ *    Week 13, Graph
+ *    Brother Helfrich, CS 235
+ * Authors
+ *    Jeffry Simpson, David Perez
+ * Summary:
+ *    the graph class
+ ************************************************************************/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <map>
+#include <vector>
+#include "list.h"
+#include "queue.h"
+#include "set.h"
+#include "vector.h"
+#include "vertex.h"
+
+class Graph
+{
+   
+};
+
+#endif //GRAPH_H
