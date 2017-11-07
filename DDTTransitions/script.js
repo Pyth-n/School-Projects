f9e25226750fc102292fde3897da6959bc332870
David Perez <3dgar.p@gmail.com>
2017-11-06 20:19:01 -0800

    Finish implementation

--- DDTTransitions/script.js
+++ DDTTransitions/script.js
@@ -1,6 +1,6 @@
 function createTransition() {
 	// See which radio button is selected
-	var shape, text, duration, methodOfTrigger;
+	var shape, text, duration, property;
 	var radios = document.getElementsByName('shape');
 	for (var i = 0; i < radios.length; i++) {
 		if (radios[i].checked) {
@@ -18,33 +18,43 @@ function createTransition() {
 	var duration = document.getElementById('inputDuration').value;
 	console.log("Duration: " + duration);
 
-	// Way to trigger
-	var triggerRadios = document.getElementsByName('trigger');
-	for (var i = 0; i < triggerRadios.length; i++) {
-		if (triggerRadios[i].checked) {
-			methodOfTrigger = triggerRadios[i].value;
-			break;
+	// Get property
+	propertyRadio = document.getElementsByName('cssProperty');
+	for (var i = 0; i < propertyRadio.length; i++) {
+		if (propertyRadio[i].checked) {
+			property = propertyRadio[i].value;
 		}
 	}
-	console.log("Trigger: " + methodOfTrigger);
+	console.log("Property: " + property);
 
 	// Create the div
 	var div = document.createElement("DIV");
-	var node = document.createTextNode(text + ": " + methodOfTrigger);
+	var node = document.createTextNode(text);
+	var nodeProp = document.createTextNode("Property: " + property);
+	var nodeShape = document.createTextNode("Shape: " + shape);
+	var nodeDur = document.createTextNode("Duration: " + duration + "s");
 	div.appendChild(node);
+	div.appendChild(document.createElement("BR"));
+	div.appendChild(nodeProp);
+	div.appendChild(document.createElement("BR"));
+	div.appendChild(nodeDur);
 	div.setAttribute("onmouseover", "forTransition(this)");
-	div.setAttribute("onmouseout", "revertTransition(this)");
 	div.setAttribute("style", "background-color: black; color: white;")
 	div.style.width = shape;
 	div.style.height = "100px";
 	document.getElementById('masterBox').appendChild(div);
-	div.style.transition = "width " + duration + "s";
-}
 
-function forTransition(val) {
+	// Special case with properties
+	if (property == "both") {
+		div.style.transition = "width " + duration + "s" + ", height " + duration + "s";
+	} else {
+		div.style.transition = property + " " + duration + "s";
+		console.log(div.style.transition = property + " " + duration + "s");
+	}
 	
 }
 
-function revertTransition(val) {
-
+function forTransition(val) {
+	val.style.width = "200px";
+	val.style.height = "200px";
 }
