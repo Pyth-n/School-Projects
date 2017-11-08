7c2bd60d1df3ee40dcace57987cda400b9c7dd53
Pyth-n <3dgar.p@gmail.com>
2017-11-07 23:12:07 -0800

    Implement function for Create Item which sends an intent to ItemActivity with the name of the list

--- app/src/main/java/com/byui_cs246_team07/listtracker/ItemActivity.java
+++ app/src/main/java/com/byui_cs246_team07/listtracker/ItemActivity.java
@@ -1,13 +1,22 @@
 package com.byui_cs246_team07.listtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ItemActivity extends AppCompatActivity {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_item);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+
+        // Gets the intent from ListActivity. Sets the name of the list
+        Intent intent = getIntent();
+        String listName = intent.getStringExtra(ListActivity.listNameID);
+
+        TextView textView = (TextView) findViewById(R.id.listNameInItemScreen);
+        textView.setText(listName);
+    }
 }
--- app/src/main/java/com/byui_cs246_team07/listtracker/ListActivity.java
+++ app/src/main/java/com/byui_cs246_team07/listtracker/ListActivity.java
@@ -1,13 +1,38 @@
 package com.byui_cs246_team07.listtracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
+    public static final String listNameID = "com.byui.cs246.team07.listtracker.LISTID";
+    private final String TAG = this.getClass().getName();
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_list);
-  }
+    // TODO get the name of the list, then modify this value. Used for current activity and intent
+    private String nameOfList = "Sample List Name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        // Sets the name of the list in the current activity
+        TextView textView = (TextView) findViewById(R.id.listNameInListScreen);
+        textView.setText(nameOfList);
+    }
+
+    /*createItem(): This function is called by the "Create Item" button. It opens up the ItemActivity
+    * */
+    public void createItem(View v) {
+        // Will send intent to the ItemActivity
+        Intent intent = new Intent(this, ItemActivity.class);
+        intent.putExtra(listNameID, nameOfList);
+
+        Log.d(TAG, "Sending " + intent.getStringExtra(listNameID));
+        startActivity(intent);
+    }
 }
