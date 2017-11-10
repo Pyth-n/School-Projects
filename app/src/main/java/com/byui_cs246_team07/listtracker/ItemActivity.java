043e3937910a8da6633c77b4d5bcd863e676a3f4
Pyth-n <3dgar.p@gmail.com>
2017-11-09 19:36:43 -0800

    Implement Load Item button that sends an intent to ItemActivity.

--- app/src/main/java/com/byui_cs246_team07/listtracker/ItemActivity.java
+++ app/src/main/java/com/byui_cs246_team07/listtracker/ItemActivity.java
@@ -3,20 +3,49 @@ package com.byui_cs246_team07.listtracker;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ItemActivity extends AppCompatActivity {
+    // IDs of widgets
+    TextView mListName;
+    EditText mItemName;
+    EditText mTag;
+    EditText mDueDate;
+    TextView mDateCreated;
+    TextView mDateModified;
+    CheckBox mCheckBox;
+    EditText mPriorityNumber;
+    EditText mPriorityLevel;
+    EditText mNotes;
+
+    // TODO get the data from the database to store in the fields whenever intent from "load" occurs
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
 
-        // Gets the intent from ListActivity. Sets the name of the list
-        Intent intent = getIntent();
-        String listName = intent.getStringExtra(ListActivity.listNameID);
+        // find resource IDs
+        mItemName = findViewById(R.id.editItemName);
+        mListName = (TextView) findViewById(R.id.listNameInItemScreen);
 
-        TextView textView = (TextView) findViewById(R.id.listNameInItemScreen);
-        textView.setText(listName);
+        // Set name of the list
+        mListName.setText(getIntent().getStringExtra(ListActivity.LIST_NAME_ID));
+
+        // Differentiate between the buttons that sent the intent
+        String buttonName = getIntent().getStringExtra(ListActivity.BUTTON_PRESSED);
+        if (buttonName.equals("createItem")) {
+            Toast.makeText(this, "CREATING...", Toast.LENGTH_SHORT).show();
+        }
+        // TODO loading magic occurs here. Not sure how to utilize database to populate from list
+        // TODO selected yet
+        else if (buttonName.equals("loadItem")) {
+            String itemName = getIntent().getStringExtra(ListActivity.ITEM_NAME_ID);
+            mItemName.setText(itemName);
+            Toast.makeText(this, "LOADING...", Toast.LENGTH_SHORT).show();
+        }
     }
 }
