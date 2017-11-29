f4cda3c3974c1320089a2122918625cb2233d541
Pyth-n <3dgar.p@gmail.com>
2017-11-28 22:59:47 -0800

    Mocking ListView in SearchResultsActivity success

--- app/src/main/java/com/byui_cs246_team07/listtracker/SearchResultsActivity.java
+++ app/src/main/java/com/byui_cs246_team07/listtracker/SearchResultsActivity.java
@@ -2,18 +2,49 @@ package com.byui_cs246_team07.listtracker;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchResultsActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private String query;
+    private ArrayAdapter<String> adapter;
+    private List<String> results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
+        Toolbar myToolbar = findViewById(R.id.search_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        listView = findViewById(R.id.search_lists);
+
+
         handleIntent(getIntent());
+        actionBar.setTitle(Html.fromHtml(
+                "<font color=\"#ffffff\">" + "Search Results: " + query + "</font>"
+        ));
+
+        results = new ArrayList<>();
+        results.add(query);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, results);
+
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -23,7 +54,7 @@ public class SearchResultsActivity extends AppCompatActivity {
 
     private void handleIntent(Intent intent) {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
+            query = intent.getStringExtra(SearchManager.QUERY);
             //use the query to search your data somehow
             Log.d("LOOL", query);
         }
