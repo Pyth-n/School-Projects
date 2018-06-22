d97f37344955da18e36607cf678e85c6e32654e0
David Perez <per16035@byui.edu>
2018-06-21 23:26:52 -0700

    Finish requirements

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
+// Setup express and ejs
 const app = require('express')();
+app.set('view engine', 'ejs');
+
+// Parse package
+const url = require('url');
+
 const PORT = process.env.PORT || 1337;
 
-app.get('/', function(req, res) {
+// Calculation export
+var computeRate = require('./pkg/computeRate');
+
+app.get('/prove09', function(req, res) {
     console.log("Curled");
-    res.end('Hello World!');
+    res.render('postal_rate_form');
 });
+
+app.get('/calculateRate', function(req, res) {
+    handleRate(req, res);
+    res.end();
+});
+
+// Get GET variables
+function getVariables(req) {
+    return url.parse(req.url, true);
+}
+
+// Handle GET rate
+function handleRate(req, res) {
+    var variables = getVariables(req);
+
+    var weight = variables.query.weight;
+    var mailType = variables.query.mailType;
+
+    computeRate(weight, mailType, res);
+}
+
+
 
 app.listen(PORT, () => console.log("Running on port: " + PORT));
