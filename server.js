575d859552fb8781a1ac808eb2e7bee5d8f7d533
David Perez <per16035@byui.edu>
2018-06-26 23:26:16 -0700

    Port to NodeJS

---
+++ server.js
@@ -0,0 +1,22 @@
+// Use express
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('imgs'));
+
+app.get('/', function(req, res) {
+    res.render('home');
+    res.end();
+});
+
+app.get('/projects', function(req, res) {
+    res.render('projects');
+    res.end();
+});
+
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+
+app.listen(1337);
