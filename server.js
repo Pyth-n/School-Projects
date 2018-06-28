a3c9bf113b3675a63836f379f89b7dd432652617
David Perez <per16035@byui.edu>
2018-06-27 23:21:57 -0700

    Pass POST data to /send

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 1337;
 // Using EJS as viewing engine
 app.set('view engine', 'ejs');
 
+// Body parser for POST data
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
 // Use these static files
 app.use(express.static('imgs'));
 app.use('/style', express.static('public'));
@@ -29,6 +38,14 @@ app.get('/projects', function(req, res) {
 // Contact
 app.get('/contact', function(req, res) {
     res.render('contact');
+});
+
+// Send message
+app.post('/send', function(req, res) {
+    const params = { email: req.body.user.email, name: req.body.user.name, message: req.body.user.message };
+
+    res.render('sent', params);
+    res.end();
 });
 
 app.listen(PORT, () => console.log("Running on port: " + PORT));
