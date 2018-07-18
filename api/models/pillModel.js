89197cce067af3d3820a710aae80fc58a2ec3f8f
David Perez <per16035@byui.edu>
2018-07-17 22:08:14 -0700

    Remove timestampz from DB. Fix insertDay bug

--- api/models/pillModel.js
+++ api/models/pillModel.js
@@ -148,19 +148,16 @@ exports.insertPill = function(id, data, cb) {
 
 // Insert days
 exports.insertDays = function(id, data, cb) {
-    console.log("ID: " + id);
-    console.log("Data: " + JSON.stringify(data));
-
-    const insertString = "INSERT INTO reminder(pill_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday) values($1,$2,$3,$4,$5,$6,$7,$8)";
+    const insertString = "INSERT INTO reminder(pill_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday) values($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *";
     const insertValues = [id, data.monday, data.tuesday, data.wednesday, data.thursday, data.friday, data.saturday, data.sunday];
 
     transaction(pool, insertString, insertValues, (err, data) => {
         console.log("DAYS: " + data);
     });
 
-    if(typeof cb === "function") {
-        cb(false);
-    }
+    // if(typeof cb === "function") {
+    //     cb(false);
+    // }
 }
 
 // Compare passwords
